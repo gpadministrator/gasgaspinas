af1d1f76345976b6fe9ca62aea161a0254cc2dfc
k0ms <mark.samaniego@gmail.com>
2015-03-26 07:53:48 +0800

    fix

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -15,17 +15,23 @@ angular.module('gasgaspinas.controllers', []).
     });
 
   }).
-  controller('UsersCtrl', function ($scope, $http) {
-    $scope.vehicles = [];/*= {
+  controller('UsersCtrl', function ($scope, $http, uiGridConstants) {
+    $scope.vehicles = {
       columnDefs: [
-        { field: 'vehicle_name', displayName: 'Name' },
-        { field: 'vehicle_type', displayName: 'Type' },
-        { field: 'fuel_type',    displayName: 'Fuel' },
-        { field: 'brand',        displayName: 'Brand' },
-        { field: 'model',        displayName: 'Model' }
+        { field: 'vehicle_name', name: 'Name' },
+        { field: 'vehicle_type', name: 'Type' },
+        { field: 'fuel_type',    name: 'Fuel' },
+        { field: 'brand',        name: 'Brand' },
+        { field: 'model',        name: 'Model' },
+        { name: 'Actions', 
+          cellTemplate: '<div class="grid-action-cell">'+
+             '<a ng-click="$event.stopPropagation(); deleteThisRow(row.entity);" href="#"><i class="icon-large icon-search"></i> Delete</a></div>'
+        }
       ],
+      enableHorizontalScrollbar:  uiGridConstants.scrollbars.NEVER, 
+      enableVerticalScrollbar:  uiGridConstants.scrollbars.ALWAYS,
       data: []
-    };*/
+    };
     $http.get('/api/users').
       success(function(data, status, headers, config) {
       $scope.data = data.data;
@@ -43,9 +49,7 @@ angular.module('gasgaspinas.controllers', []).
       $http.get('/api/vehicles/'+item._id).
         success(function(data, status, headers, config) {
         console.log("VEHICLES: "+JSON.stringify(data.data));
-        //$scope.vehicles ={data:  data.data};  
-        $scope.vehicles = data.data;  
-
+        $scope.vehicles ={data:  data.data};  
         console.log("INFO: "+ JSON.stringify($scope.vehicles));
       });
     }
